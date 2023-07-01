0b2a7efd623dee16702ca2b952c96ea92434ed75
Janet <nope.janet.97@gmail.com>
2023-06-30 22:47:56 -0500

    3commint

--- nosotros.js
+++ nosotros.js
@@ -1,11 +1,35 @@
 
 
- const collapsible  = document.querySelectorAll("toggle")
+const buttonOfrecemos = document.querySelector('#ofrecemosid')
 
- collapsible.forEach(button => {
-    button.addEventListener('click', () => {
-        button.parentNode.classList.toggle('active')
-    })
- });
+const parrafOfrecemos = document.querySelector('#section-content-header2-id')
+
+buttonOfrecemos.addEventListener('click', () =>{
+    parrafOfrecemos.className = 'active'
+})
+
+
+
+const buttoncreo =document.querySelector('#creoid');
+console.log(buttoncreo);
+
+const parrafocreo = document.querySelector('#section-content-header3-id')
+console.log(parrafocreo);
+
+buttoncreo.addEventListener('click', ()=>{
+    parrafocreo.className = 'active'
+})
+
+
+const buttonactualidad =document.querySelector('#actualidad-id')
+console.log(buttonactualidad)
+
+const parrafoactualidad =document.querySelector('#section-content-header4-id')
+console.log(parrafoactualidad)
+
+buttonactualidad.addEventListener('click', () => {
+    parrafoactualidad.className = 'active'
+}
+)
 
 
